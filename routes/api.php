1ed07ab95955b332a4c31810093e346dd09505dc
Khai Rahman <khairahmanplus@gmail.com>
2018-11-02 11:21:24 +0800

    Add login routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
 Route::prefix('v1')->namespace('API\V1')->name('api.')->group(function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::get('me', 'AuthController@me');
     Route::apiResource('articles', 'ArticleController');
 });

b31188f7455b9225c41e956a9a2e3b3cb25fe9b5
Khai Rahman <khairahmanplus@gmail.com>
2018-11-02 11:52:27 +0800

    Add name to routes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 Route::prefix('v1')->namespace('API\V1')->name('api.')->group(function () {
-    Route::post('login', 'AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::get('me', 'AuthController@me');
+    Route::post('login', 'AuthController@login')->name('login');
+    Route::post('logout', 'AuthController@logout')->name('logout');
+    Route::post('refresh', 'AuthController@refresh')->name('refresh');
+    Route::get('me', 'AuthController@me')->name('me');
     Route::apiResource('articles', 'ArticleController');
 });
